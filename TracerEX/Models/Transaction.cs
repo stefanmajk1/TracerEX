acec747d22df011c2d00254a7ff02f98f568c6e7
stefanmajk1 <stefanmajkic00@gmail.com>
2022-08-08 20:30:47 +0200

    Add project files.

---
+++ TracerEX/Models/Transaction.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TracerEX.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage ="Please select a category.")]
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }//foreign key
+
+        [Range(1, int.MaxValue, ErrorMessage = "Amount should not be null.")]
+        public int Amount { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string? Note { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        [NotMapped]
+        public string? CategoryTitleWithIcong
+        {
+            get
+            {
+                return Category == null ? "" : Category.Icon + " " + Category.Title;
+            }
+        }
+        [NotMapped]
+        public string? FormattedAmount
+        {
+            get
+            {
+                return ((Category == null || Category.Type == "Expense") ? "-" :"+") + Amount.ToString("C0");
+            }
+        }
+
+    }
+}
